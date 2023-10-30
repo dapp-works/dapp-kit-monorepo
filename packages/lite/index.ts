2bd1d73f4ba1a0551405814d82394a23b191b957
uu-z <ncysatnaf@gmail.com>
2023-10-30 18:26:57 +0800

    add lite
    dappworks/lite

---
+++ packages/lite/index.ts
@@ -0,0 +1,8 @@
+// export { type DefaultEventMap, rootStore, useStore } from "./store";
+// export { PromiseState } from "./store/standard/PromiseState";
+export { UserStore } from "./store/user";
+export * from "./store/standard/base";
+export { ToastPlugin } from "./module/Toast/Toast";
+export { RootStore } from "./store/root";
+export { AppProvider } from "./module/AppProvider";
+export * from "./lib/helper";
---
+++ packages/lite/module/Toast/Toast.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+import { Store } from "../../store/standard/base";
+
+export class ToastPlugin implements Store {
+  sid = "ToastPlugin";
+  provider = () => <Toaster />;
+
+  success = toast.success;
+  error = toast.error;
+  loading = toast.loading;
+  custom = toast.custom;
+  dismiss = toast.dismiss;
+  remove = toast.remove;
+  promise = toast.promise;
+}
