7c0741d7863be199df49fa2c85a01da88fb807d6
osdodo <osdodo@qq.com>
2023-11-07 11:38:30 +0800

    update SelectWidget

--- packages/dappkit/components/JSONFormWidgets/SelectWidget/index.tsx
+++ packages/dappkit/components/JSONFormWidgets/SelectWidget/index.tsx
@@ -18,18 +18,13 @@ export interface SelectWidgetUIOptions {
   "ui:options": Options;
 }
 
-function SelectWidget({
-  onChange,
-  options,
-  id,
-  label,
-  value,
-  required,
-  disabled,
-  schema,
-}: SelectWidgetProps) {
-  const { className, labelPlacement = "top", placeholder = 'Select an option' } = options;
+function SelectWidget(props: SelectWidgetProps) {
+  const { onChange, options, id, label, value, required, disabled, schema } = props;
+  const { className, labelPlacement = 'top', placeholder = 'Select an option' } = options;
   const { selectOptions = [], description } = schema;
+  const labelText = label?.trim();
+  const placeholderText = props.placeholder || placeholder;
+
   return (
     <div className={cn("", className)}>
       <div
@@ -38,19 +33,21 @@ function SelectWidget({
         })}
         id={id}
       >
-        {label && (
+        {labelText && (
           <label
             className={cn("flex items-center text-sm", {
               "mb-2": labelPlacement === "top",
               "mr-2": labelPlacement === "left",
             })}
           >
-            {label}
-            {required && <span className="font-bold text-red-600">*</span>}
+            {labelText}
+            {required && <span className="ml-[2px] font-bold text-red-600">*</span>}
           </label>
         )}
-        <select className="w-full p-3 text-sm rounded-md bg-[#F4F4F5] dark:bg-[#27272A]" defaultValue={value} onChange={(event) => onChange(event.target.value)}>
-          <option value="" disabled selected>{placeholder}</option>
+        <select className="w-full p-3 text-sm rounded-md bg-[#F4F4F5] dark:bg-[#27272A]" defaultValue={value} disabled={disabled} onChange={(event) => onChange(event.target.value)}>
+          <option value="" disabled selected>
+            {placeholderText}
+          </option>
           {selectOptions.map((item) => {
             return (
               <option key={item.value} value={item.value}>
@@ -60,7 +57,7 @@ function SelectWidget({
           })}
         </select>
       </div>
-      {description && <div className="mb-2 text-xs">{description}</div>}
+      {description && <div className="mt-1 text-xs text-[#A1A1A9] dark:text-[#717179]">{description}</div>}
     </div>
   );
 }
