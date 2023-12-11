ec672bfe71df32fd213a5db93a947e0f853b9914
DLHTX <949729789@qq.com>
2023-12-11 15:07:05 +0800

    update

--- packages/dappkit/index.ts
+++ packages/dappkit/index.ts
@@ -1,6 +1,6 @@
 export { type DefaultEventMap, rootStore, useStore } from "./store";
 export { PromiseState } from "./store/standard/PromiseState";
-// export { UserStore } from "./store/user";
+export { UserStore } from "./store/user";
 export * from "./store/standard/base";
 export { RootStore } from "./store/root";
 export { AppProvider } from "./module/AppProvider";
