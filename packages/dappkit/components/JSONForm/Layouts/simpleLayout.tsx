e995e281e30ec58dd106a858eef72df968b3903b
osdodo <osdodo@qq.com>
2024-02-18 12:03:53 +0800

    update JSONForm

--- packages/dappkit/components/JSONForm/Layouts/simpleLayout.tsx
+++ packages/dappkit/components/JSONForm/Layouts/simpleLayout.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FormDataType, JSONFormProps, LayoutConfigType } from '..';
 import { JSONSchemaForm } from '../../../components/JSONSchemaForm';
 import { BatchSubmitButton, SubmitButton, getFormState } from './format';
 
 export const SimpleLayout = <T extends FormDataType>(props: JSONFormProps<T>) => {
-  const { layoutConfig = {}, onBatchSubmit, batchSubmitButtonProps } = props;
+  const { layoutConfig = {}, onBatchSubmit, batchSubmitButtonProps, onReady } = props;
   const { $type, ...formLayout } = layoutConfig as LayoutConfigType<T, 'SimpleLayout'>;
   const formStates = getFormState(props, formLayout);
+
+  useEffect(() => {
+    if (formStates && onReady) {
+      onReady(formStates);
+    }
+  }, [formStates]);
 
   return (
     <>
       {Object.keys(formStates).map((key) => {
         const layout = formLayout[key];
         return (
-          <div key={key} className="space-y-2">
+          <div key={key} className="space-y-2" id={`form-${key}`}>
             <JSONSchemaForm formState={formStates[key]}>{layout?.submitButtonProps && <SubmitButton formKey={key} formState={formStates[key]} buttonProps={layout.submitButtonProps} />}</JSONSchemaForm>
           </div>
         );
