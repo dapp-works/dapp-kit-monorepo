abd179334a4edeea8ff4ecbe001bb2f78079ce39
uu-z <ncysatnaf@gmail.com>
2024-02-29 20:00:51 +0800

    update

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -7,7 +7,7 @@ const { nextui } = require("@nextui-org/react");
 export default {
   content: [
     "./src/**/*.{ts,tsx}",
-    "./node_modules/@dappworks/kit/**/*.{js,ts,jsx,tsx}",
+    // "./node_modules/@dappworks/kit/**/*.{js,ts,jsx,tsx}",
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
     '../../node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
