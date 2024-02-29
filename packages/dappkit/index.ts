28ede386319ec0468b8fe0d9bd85643659da6e6b
DLHTX <949729789@qq.com>
2024-02-29 13:11:17 +0800

    update

--- packages/dappkit/index.ts
+++ packages/dappkit/index.ts
@@ -2,6 +2,7 @@ export { type DefaultEventMap, rootStore, useStore } from "./store";
 export { PromiseState } from "./store/standard/PromiseState";
 export { PromiseHook } from "./store/standard/PromiseHook";
 export { BigNumberState } from "./store/standard/BigNumberState";
+export { BigNumberInputState } from "./store/standard/BigNumberInputState";
 export { UserStore } from "./store/user";
 export * from "./store/standard/base";
 export { RootStore } from "./store/root";
