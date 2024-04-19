44bb2fb90968138c574c198755992f2b0b26c2e7
osdodo <anonymous@localhost>
2024-04-19 16:16:30 +0800

    update metrics module

--- packages/dappkit/components/JSONMetricsView/BarChartCard/index.tsx
+++ packages/dappkit/components/JSONMetricsView/BarChartCard/index.tsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import { BarChart, ValueFormatter } from '@tremor/react';
 import { ChartBox } from '../ChartBox';
+import { cn } from '../../../lib/utils';
 
 export type BarChartCard = ChartBox & {
-  type?: 'BarChartCard',
+  type?: 'BarChartCard';
   categories?: string[];
   index?: string;
   valueFormatter?: ValueFormatter;
   showLegend?: boolean;
   showGridLines?: boolean;
   stack?: boolean;
-}
+  showAnimation?: boolean;
+};
 
 export const BarChartCard = (props: BarChartCard) => {
   const {
-    data = [], categories = ['value'], index = 'date', valueFormatter = (number) => `${number}`,
-    showLegend = false, showGridLines = false, stack = false
+    data = [],
+    categories = ['value'],
+    index = 'date',
+    valueFormatter = (number) => `${number}`,
+    showLegend = false,
+    showGridLines = false,
+    stack = false,
+    chartClassName,
+    showAnimation = true,
   } = props;
 
   return (
     <ChartBox {...props}>
       <BarChart
-        className="h-72 mt-4"
+        className={cn('h-72 mt-4', chartClassName)}
         data={data}
         index={index}
         categories={categories}
-        colors={["indigo", "cyan", "teal", "green", "yellow", "orange", "red", "slate", "violet", "rose", "pink", "purple", "blue"]}
+        colors={['indigo', 'cyan', 'teal', 'green', 'yellow', 'orange', 'red', 'slate', 'violet', 'rose', 'pink', 'purple', 'blue']}
         valueFormatter={valueFormatter}
         yAxisWidth={48}
         showLegend={showLegend}
         showGridLines={showGridLines}
         stack={stack}
+        showAnimation={showAnimation}
       />
     </ChartBox>
   );
