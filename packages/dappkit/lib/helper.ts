12af556df1c69bc69d83f8260d0752d811c9863d
Jevin_F <jevinfsmail@gmail.com>
2024-04-22 17:31:05 +0800

    update helper

--- packages/dappkit/lib/helper.ts
+++ packages/dappkit/lib/helper.ts
@@ -1,8 +1,10 @@
-import BN from "bignumber.js";
+import BN, { BigNumber } from "bignumber.js";
 import JSONFormat from "json-format";
 import { _ } from "./lodash";
 import copy from "copy-to-clipboard";
 import numeral from "numeral";
+import { from } from "@iotexproject/iotex-address-ts";
+import { BigNumberState } from "../store/standard/BigNumberState";
 
 const valMap = {
   undefined: "",
@@ -235,5 +237,88 @@ export const helper = {
 
       return new BN(result).toFixed();
     },
+
+    //format: '0,0'
+    warpBigNumber(value: string, decimals = 18, options?: { format?: string; fallback?: string; min?: number }) {
+      const { format = "0.0", fallback = "0.000", min } = options || {};
+      if (!value) {
+        return {
+          value: "...",
+          format: "...",
+          decimals: "0",
+          isZero: true,
+        };
+      }
+      const BigNumberResponse = new BigNumberState({ value: new BigNumber(value), decimals });
+      return {
+        value: BigNumberResponse.value.toFixed(0),
+        originFormat: BigNumberResponse.value.div(10 ** decimals).toFixed(),
+        format: helper.number.numberFormat(BigNumberResponse.value.div(10 ** decimals).toFixed(), format, { fallback, min }),
+        decimals: String(BigNumberResponse.decimals),
+        isZero: BigNumberResponse.value.isZero(),
+      };
+    },
+    //http://numeraljs.com/ format params does not need to deal with decimal places
+    //format: '$0,0' '0a' '0,0' '0,0$'
+    numberFormat(str: string | number, format: string = "0,0", options: { min?: number; fallback?: string } = {}): string {
+      const { fallback = "0.00" } = options || {};
+
+      if (!str || isNaN(Number(str))) return fallback;
+      const numStr = new BigNumber(str).toFixed();
+      const countNonZeroNumbers = (_str: string) => {
+        const decimalPointIndex = _str.indexOf(".");
+        if (decimalPointIndex === -1) {
+          return 0;
+        }
+        const decimalPart = _str.substring(decimalPointIndex + 1);
+        let trailingZerosCount = 0;
+        for (let i = 0; i < decimalPart.length; i++) {
+          if (decimalPart[i] === "0") {
+            trailingZerosCount++;
+          } else {
+            break;
+          }
+        }
+        return trailingZerosCount;
+      };
+
+      const fractionDigits = countNonZeroNumbers(format);
+      const numberFractionDigits = countNonZeroNumbers(numStr);
+      if (options?.min) {
+        if (new BigNumber(numStr).isLessThan(new BigNumber(options?.min || 0))) {
+          return `< ${numeral(options?.min).format(format)}`;
+        }
+      }
+      const fullStr = new BigNumber(numStr).toFixed();
+      let preStr = numeral(fullStr.split(".")[0]).format(format.split(".")[0]);
+      const fractionStr = fullStr.split(".")?.[1]?.slice(0, fractionDigits + numberFractionDigits);
+
+      if (numberFractionDigits >= fractionDigits) {
+        return (preStr + "." + fractionStr).replace(/\.?0+$/, "");
+      }
+
+      if (fractionStr?.[fractionDigits - 1] == "9") {
+        return (preStr + "." + fractionStr.slice(0, fractionDigits - 1) + "9").replace(/\.?0+$/, "");
+      }
+
+      const resultStr = numeral(new BigNumber(numStr).toString()).format(format);
+      return resultStr.replace(/\.?0+$/, "");
+    },
+  },
+  address: {
+    convertAddress: (addressMode: "io" | "0x", address: string = ""): string => {
+      try {
+        if (addressMode == "0x" && helper.address.validateIoAddress(address)) {
+          return from(address).stringEth();
+        }
+        if (addressMode == "io" && helper.address.validateEthAddress(address)) {
+          return from(address).string();
+        }
+      } catch (error) {}
+      return address;
+    },
+    validateEthAddress: (address: string) => /^0x[a-fA-F0-9]{40}$/.test(address),
+    validateIoAddress: (address: string) => /^io[a-zA-Z0-9]{39}$/.test(address),
+    validateAddress: (address: string) => helper.address.validateEthAddress(address) || helper.address.validateIoAddress(address),
   },
 };
