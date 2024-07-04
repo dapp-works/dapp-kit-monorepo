49c4059f6aa923b6d9faf3a48a08ace993128575
uu-z <ncysatnaf@gmail.com>
2024-07-04 17:24:27 +0800

    update v0.4.102
    # Conflicts:
    #	packages/dappkit/package.json

--- packages/dappkit/aiem.ts
+++ packages/dappkit/aiem.ts
@@ -1,8 +1,7 @@
-import { type Chain, type GetContractReturnType, createPublicClient, getContract, http, type Abi, PublicClient, HttpTransport, WalletClient } from 'viem'
-import QuickLRU from 'quick-lru';
+import { type Chain, type GetContractReturnType, createPublicClient, getContract, http, type Abi, PublicClient, HttpTransport, WalletClient, AbiFunction } from 'viem'
 import md5 from "md5"
-import { iotex, mainnet, bsc, polygon } from 'viem/chains'
-
+import { iotex, mainnet, bsc, polygon, iotexTestnet, } from 'viem/chains'
+import TTLCache from '@isaacs/ttlcache'
 
 //@ts-ignore
 mainnet.rpcUrls.default.http = ['https://rpc.ankr.com/eth']
@@ -13,62 +12,88 @@ mainnet.rpcUrls.default.webSocket = ["wss://ethereum-rpc.publicnode.com"]
 
 
 export class Cache {
-  kv = new QuickLRU<string, any>({ maxSize: 10000 });
+  kv = new TTLCache<string, any>({ max: 10000, ttl: 1000 * 60, });
 
-  wrap<T>(key: string, fn: () => T | Promise<T>): T | Promise<T> {
+  wrap<T>(key: string, fn: () => T | Promise<T>, config: TTLCache.Options<any, any> = {}): T | Promise<T> {
     if (this.kv.has(key)) {
+      console.log(`load ${key} from cache`)
       return this.kv.get(key);
     }
 
     const result = fn();
     if (result instanceof Promise) {
       const promiseResult = result.then(res => {
-        this.kv.set(key, res);
+        this.kv.set(key, res, config);
         return res;
       });
-      this.kv.set(key, promiseResult);
+      this.kv.set(key, promiseResult, config);
       return promiseResult;
     } else {
-      this.kv.set(key, result);
+      this.kv.set(key, result, config);
       return result;
     }
   }
 }
 
+
 export class AIem<Contracts extends Record<string, Abi>, Chains extends Record<string, Chain>, Addrs extends { [K in keyof Contracts]?: { [key: string]: `${string}-0x${string}` } }> {
   static cache?: Cache = new Cache()
-  contractMap: Contracts
+  cache?: Cache = new Cache()
+  contractMap?: Contracts
   //@ts-ignore
   chainMap?: Chains = {
     [iotex.id]: iotex,
     [mainnet.id]: mainnet,
     [bsc.id]: bsc,
-    [polygon.id]: polygon
+    [polygon.id]: polygon,
+    [iotexTestnet.id]: iotexTestnet,
   }
-  nameMap: Addrs
+  nameMap?: Addrs
   contracts: {
     [K in keyof Addrs & keyof Contracts]: {
       //@ts-ignore
       [KK in keyof Addrs[K]]: GetContractReturnType<Contracts[K], PublicClient<HttpTransport, Chain, any, any>, any, any>
     }
   }
+  funcMap?: { [key: string]: { ttl?: number } } = {
+    "totalSupply": { ttl: 15 },
+    "symbol": { ttl: 60 },
+    "name": { ttl: 60 },
+    "decimals": { ttl: 60 },
+    "balanceOf": { ttl: 5 }
+  }
 
-  get cache() {
+
+  get _cache() {
     return AIem.cache
   }
 
+
+  //@ts-ignore
   getWallet?: () => WalletClient
 
-  constructor(args: Pick<AIem<Contracts, Chains, Addrs>, "contractMap" | "chainMap" | "nameMap" | "getWallet">) {
+  static Set<Contracts extends Record<string, Abi>, Chains extends Record<string, Chain>, Addrs extends { [K in keyof Contracts]?: { [key: string]: `${string}-0x${string}` } }>(args: Pick<AIem<Contracts, Chains, Addrs>, "contractMap" | "chainMap" | "nameMap" | "getWallet" | "cache" | "funcMap">) {
+    return this.init().Set(args)
+  }
 
-    const { chainMap = {}, contractMap = {}, ...rest } = args || {}
+  Set<Contracts extends Record<string, Abi>, Chains extends Record<string, Chain>, Addrs extends { [K in keyof Contracts]?: { [key: string]: `${string}-0x${string}` } }>(args: Pick<AIem<Contracts, Chains, Addrs>, "contractMap" | "chainMap" | "nameMap" | "getWallet" | "cache" | "funcMap">): AIem<Contracts, Chains, Addrs> {
+    const { chainMap = {}, contractMap = {}, funcMap, ...rest } = args || {}
     //@ts-ignore
     this.chainMap = Object.assign({}, this.chainMap || {}, chainMap)
     //@ts-ignore
     this.contractMap = Object.assign({}, this.contractMap || {}, contractMap)
+    //@ts-ignore
+    this.funcMap = Object.assign({}, this.funcMap || {}, funcMap)
 
     Object.assign(this, rest)
+    return this as any
+  }
 
+
+  constructor(args: Pick<AIem<Contracts, Chains, Addrs>, "contractMap" | "chainMap" | "nameMap" | "getWallet" | "cache" | "funcMap"> = {}) {
+
+
+    this.Set(args)
 
     this.contracts = new Proxy({}, {
       //@ts-ignore
@@ -116,7 +141,7 @@ export class AIem<Contracts extends Record<string, Abi>, Chains extends Record<s
 
   PubClient<C extends keyof Chains>(chainId: C): PublicClient<HttpTransport, Chain, any, any> {
     //@ts-ignore
-    return this.cache.wrap(`publicClient-${String(chainId)}`, () => {
+    return this._cache.wrap(`publicClient-${String(chainId)}`, () => {
       //@ts-ignore
       return createPublicClient({
         //@ts-ignore
@@ -132,14 +157,14 @@ export class AIem<Contracts extends Record<string, Abi>, Chains extends Record<s
     const wallet = this.getWallet ? this.getWallet() : null
     //@ts-ignore
     const cacheKey = `contract: ${contractName}-${chainId}-${address}-${wallet ? wallet.account.address : wallet}`
-    return this.cache.wrap(cacheKey, () => {
+    return this._cache.wrap(cacheKey, () => {
       //@ts-ignore
       const contract = this.contractMap[contractName];
       //@ts-ignore
       const pubClient = this.PubClient(chainId)
 
       //@ts-ignore
-      return getContract({
+      return this.getContract({
         client: {
           //@ts-ignore
           public: pubClient,
@@ -150,6 +175,48 @@ export class AIem<Contracts extends Record<string, Abi>, Chains extends Record<s
         abi: contract
       })
     }) as any
+  }
+
+  getContract({
+    client,
+    address,
+    abi
+  }: {
+    client: {
+      public: PublicClient<HttpTransport, Chain, any, any>
+      wallet?: WalletClient
+    }
+    address: `0x${string}`
+    abi: any,
+  }) {
+    const handler = {
+      get: (target: any, funcName: any) => {
+        if (typeof target[funcName] === 'function') {
+          return async (...args: any[]) => {
+            const methodConfig = this.funcMap?.[funcName as string];
+            const cacheKey = `method:${client.public.chain.id}-${address}-${String(funcName)}-${JSON.stringify(args)}`;
+
+            if (methodConfig) {
+              return this.cache.wrap(cacheKey, () => {
+                return target[funcName](...args);
+              }, methodConfig);
+            }
+
+            return target[funcName](...args);
+          };
+        }
+        return new Proxy(target[funcName], handler)
+      }
+    }
+
+    //@ts-ignore
+    const contract = getContract({
+      //@ts-ignore
+      client,
+      address,
+      abi
+    })
+    return new Proxy(contract, handler) as any
   }
 
 
@@ -165,13 +232,14 @@ export class AIem<Contracts extends Record<string, Abi>, Chains extends Record<s
   //@ts-ignore
   static Get<TAbi extends Abi = any>(abi: TAbi, chainId: any, address: any, wallet?: WalletClient): GetContractReturnType<TAbi, PublicClient<HttpTransport, Chain, any, any>> {
     const aiem = this.init()
+
     const cacheKey = `contract ${md5(JSON.stringify(abi))}-${chainId}-${address}-${wallet ? wallet.account.address : null}`
-    return aiem.cache.wrap(cacheKey, () => {
+    return aiem._cache.wrap(cacheKey, () => {
       //@ts-ignore
       const pubClient = aiem.PubClient(chainId)
 
       //@ts-ignore
-      return getContract({
+      return aiem.getContract({
         client: {
           //@ts-ignore
           public: pubClient,
@@ -184,3 +252,12 @@ export class AIem<Contracts extends Record<string, Abi>, Chains extends Record<s
     }) as any
   }
 }
+
+
+export type ReadFunctionKeys<T extends Abi> = T[number] extends infer U
+  ? U extends AbiFunction
+  ? U['stateMutability'] extends 'view' | 'pure'
+  ? U['name']
+  : never
+  : never
+  : never;
