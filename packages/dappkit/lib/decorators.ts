ae3565a2f2e73efcb8e357c004267b286c9dfc24
uu-z <ncysatnaf@gmail.com>
2024-07-05 17:40:17 +0800

    update @0.4.107

--- packages/dappkit/lib/decorators.ts
+++ packages/dappkit/lib/decorators.ts
@@ -5,8 +5,7 @@ export const FIELD_KEY = Symbol('aiem_field');
 
 
 
-export interface ReadParams {
-    foo?: string
+export interface FieldParams {
 }
 
 
@@ -15,25 +14,30 @@ export type ContractParams<T extends any = any, K extends keyof T = keyof T> = K
 
 
 export class Fields {
-    static read(options: ReadParams = {}) {
-        return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
-            Reflect.defineMetadata(FIELD_KEY, { type: 'read', options }, target, propertyKey);
-        };
-    }
+  static read(options: FieldParams = {}) {
+    return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
+      Reflect.defineMetadata(FIELD_KEY, { type: 'read', options }, target, propertyKey);
+    };
+  }
+  static write(options: FieldParams = {}) {
+    return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
+      Reflect.defineMetadata(FIELD_KEY, { type: 'write', options }, target, propertyKey);
+    };
+  }
 
-    static custom(options: ReadParams = {}) {
-        return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
-            Reflect.defineMetadata(FIELD_KEY, { type: 'custom', options }, target, propertyKey);
-        };
-    }
+  static custom(options: FieldParams = {}) {
+    return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
+      Reflect.defineMetadata(FIELD_KEY, { type: 'custom', options }, target, propertyKey);
+    };
+  }
 
-    static contract<T = any, R = any>(entity: () => ClassType<R>, options: ContractParams<T>) {
-        return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
-            Reflect.defineMetadata(FIELD_KEY, { type: 'contract', entity, targetKey: options }, target, propertyKey);
-        };
-    }
+  static contract<T = any, R = any>(entity: () => ClassType<R>, options: ContractParams<T>) {
+    return function (target: any, propertyKey: any, descriptor?: PropertyDescriptor) {
+      Reflect.defineMetadata(FIELD_KEY, { type: 'contract', entity, targetKey: options }, target, propertyKey);
+    };
+  }
 }
 
 export function getFieldMetadata(target: any, propertyKey: string) {
-    return Reflect.getMetadata(FIELD_KEY, target, propertyKey);
+  return Reflect.getMetadata(FIELD_KEY, target, propertyKey);
 }
