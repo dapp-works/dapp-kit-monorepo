49538417b4ddc43a5c17747b8ab2dcc854efcac5
osdodo <anonymous@localhost>
2024-08-06 17:17:55 +0800

    update 0.4.147

--- packages/dappkit/form.ts
+++ packages/dappkit/form.ts
@@ -4,5 +4,3 @@ export { JSONSchemaForm } from "./components/JSONSchemaForm";
 
 export { ComplexFormModalStore, getComplexFormData } from "./module/ComplexFormModal";
 export { FormModalStore, getFormData, getFormState, getSimpleFormData } from "./module/FormModal";
-
-export { type JSONDataType, JSONViewPlugin, type JSONViewType } from "./module/JSONView";
