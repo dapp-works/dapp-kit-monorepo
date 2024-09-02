99c469a1dd20fc95b5a8963a5d22787ddbb473c9
uu-z <ncysatnaf@gmail.com>
2024-09-02 20:57:54 +0800

    0.4.172

--- packages/dappkit/tests/aiem/advanced.ts
+++ packages/dappkit/tests/aiem/advanced.ts
@@ -63,43 +63,41 @@ class IUniswapV2LPEntity extends UniswapV2LPEntity {
 
 class IERC20Entity extends ERC20Entity {
 
-  @Fields.custom()
   async _totalSupply(a: string) {
     const [value, decimals] = await Promise.all([this.contract.read.totalSupply(), this.contract.read.decimals()])
     return AIem.utils.autoFormat({ value: value.toString(), decimals, chainId: this.chainId, address: this.address })
   }
-  @Fields.custom()
   async _balanceOf(address: any) {
     const [value, decimals] = await Promise.all([this.contract.read.balanceOf([address]), this.contract.read.decimals()])
     return AIem.utils.autoFormat({ value: value.toString(), decimals, chainId: this.chainId, address: this.address })
   }
-  @Fields.custom()
   async foo() {
     return 123
   }
 }
 
 
-const test = async () => {
-  let user = null
-  const res = await Promise.all(["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "0x123"].map(i => AIem.Query(IUniswapV2LPEntity, {
-    address: true,
-    totalSupply: true,
-    balanceOf: user ? [user] : false,
-    TokenMany: {
-      _totalSupply: ["test"],
-      _balanceOf: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa"],
-      approve: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "1000000000000000000000000"],
-      foo: true
-    }
-  })({ address: i, chainId: "4689", })))
 
-  console.log(res)
+const main = async () => {
+  // let user = null
+  // const res = await Promise.all(["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "0x123"].map(i => AIem.Query(IUniswapV2LPEntity, {
+  //   totalSupply: true,
+  //   balanceOf: user ? [user] : false,
+  //   TokenMany: {
+  //     _totalSupply: ["test"],
+  //     _balanceOf: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa"],
+  //     approve: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "1000000000000000000000000"],
+  //     foo: true
+  //   }
+  // })({ address: i, chainId: "4689", })))
+
+  // console.log(JSON.stringify(res, null, 2))
+
 
 }
 
 // setInterval(() => {
-test()
+// main()
 // }, 100)
 // const resArr = await AIem.QueryMany(IUniswapV2LPEntity, {
 //   totalSupply: true,
@@ -110,3 +108,23 @@ test()
 //   }
 // })([{ address: "0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", chainId: "4689", }])
 // console.log(resArr[0])
+
+
+class Test {
+  foo = 1
+  async bar(val: any) {
+    return val
+  }
+  async bar1() {
+    return console.log("call bar1")
+  }
+}
+
+const test = await AIem.Query(Test, {
+  foo: true,
+  bar: [234],
+})({ foo: 123, })
+
+console.log(test)
+test.bar1()
+
