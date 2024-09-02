0f411df13fdfbb2945ada42fde9d1b6834344d0d
uu-z <ncysatnaf@gmail.com>
2024-09-02 23:38:13 +0800

    clean up code

--- packages/dappkit/tests/aiem/advanced_contract.ts
+++ packages/dappkit/tests/aiem/advanced_contract.ts
@@ -79,25 +79,23 @@ class IERC20Entity extends ERC20Entity {
 
 
 const main = async () => {
-  // let user = null
-  // const res = await Promise.all(["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "0x123"].map(i => AIem.Query(IUniswapV2LPEntity, {
-  //   totalSupply: true,
-  //   balanceOf: user ? [user] : false,
-  //   TokenMany: {
-  //     _totalSupply: ["test"],
-  //     _balanceOf: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa"],
-  //     approve: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "1000000000000000000000000"],
-  //     foo: true
-  //   }
-  // })({ address: i, chainId: "4689", })))
+  let user = null
+  const res = await Promise.all(["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "0x123"].map(i => AIem.Query(IUniswapV2LPEntity, {
+    totalSupply: true,
+    balanceOf: user ? [user] : false,
+    TokenMany: {
+      _totalSupply: ["test"],
+      _balanceOf: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa"],
+      approve: ["0xa41412dafd1f1c0ae90f9fe7f137ea10a1bb5daa", "1000000000000000000000000"],
+      foo: true
+    }
+  })({ address: i, chainId: "4689", })))
 
-  // console.log(JSON.stringify(res, null, 2))
-
-
+  console.log(JSON.stringify(res, null, 2))
 }
 
 // setInterval(() => {
-// main()
+main()
 // }, 100)
 // const resArr = await AIem.QueryMany(IUniswapV2LPEntity, {
 //   totalSupply: true,
@@ -110,58 +108,3 @@ const main = async () => {
 // console.log(resArr[0])
 
 
-const users = [
-  { id: 1, firstName: "test1", lastName: "test1" },
-  { id: 2, firstName: "test2", lastName: "test2" },
-]
-const posts = [
-  { title: "Test Post", user_Id: 1 },
-  { title: "Test Post 2", user_Id: 2 },
-]
-
-class Post {
-  title: string
-  user_Id: number
-
-  @Fields.relation(() => User, async (e: Post) => new Promise(res => setTimeout(() => res({ data: users.find(i => i.id === e.user_Id) }), 1000)))
-  user: User
-
-  constructor(args: Partial<Post>) {
-    Object.assign(this, args)
-    this.user = new User({ data: users.find(i => i.id === this.user_Id) })
-  }
-}
-
-class User {
-  @Fields.custom(async (e: User) => new Promise(res => setTimeout(() => res(users.find(i => i.id === e.data.id)), 1000)))
-  data = {
-    id: 1,
-    firstName: "",
-    lastName: "",
-  }
-
-  @Fields.relation(() => Post, async (e: User) => new Promise(res => setTimeout(() => res(posts.filter(i => i.user_Id === e.data.id)), 1000)))
-  posts: Post[] = []
-
-  get fullName() {
-    return `${this.data.firstName} ${this.data.lastName}`
-  }
-
-  constructor(args: Partial<User>) {
-    Object.assign(this, args)
-  }
-
-}
-
-
-
-const test = new User({ data: { id: 2, firstName: "test", lastName: "test" }, posts: [new Post({ title: "Test Post", user_Id: 2 })] })
-
-const test1 = await AIem.Query(User, {
-  data: true,
-  posts: {
-    user: true
-  }
-})({})
-console.log(test.fullName, test.posts)
-console.log(test1.fullName, test1.posts)
