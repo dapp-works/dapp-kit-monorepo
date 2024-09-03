50bcc16cd169b6b1dccfa047a83140aa6949e674
DLHTX <949729789@qq.com>
2024-09-03 14:55:50 +0800

    feat: add iotex and iotexTestnet support in wallet module

--- packages/dappkit/wallet.ts
+++ packages/dappkit/wallet.ts
@@ -1,3 +1,4 @@
+
 export { WalletProvider } from './module/Wallet/provider'
 export { WalletStore } from './module/Wallet'
 export { WalletHistoryStore, WalletRpcStore } from './module/Wallet/walletPluginStore'
