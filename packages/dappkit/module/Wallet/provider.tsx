ef4756b122f90d079c310e77c74b6b1fbde3b4f2
DLHTX <949729789@qq.com>
2024-09-09 18:32:23 +0800

    refactor: remove unnecessary console.log in WalletStore

--- packages/dappkit/module/Wallet/provider.tsx
+++ packages/dappkit/module/Wallet/provider.tsx
@@ -45,7 +45,7 @@ export const WalletProvider = (({
   }, [appName, compatibleMode])
   return (
     //@ts-ignore
-    <WagmiProvider config={config} reconnectOnMount={walletConfig.reconnectOnMount}>
+    <WagmiProvider config={config} reconnectOnMount={true}>
       <QueryClientProvider client={queryClient} >
         <RainbowKitProvider locale="en" theme={theme == 'dark' ? darkTheme() : lightTheme()}>
           {/* @ts-ignore */}
