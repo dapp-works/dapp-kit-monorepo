96423ffe9e46bbff55621871f5ff9845be9b48f3
Jevin_F <jevinfsmail@gmail.com>
2024-09-10 18:13:39 +0800

    update promiseState

--- packages/dappkit/store/standard/PromiseState.ts
+++ packages/dappkit/store/standard/PromiseState.ts
@@ -25,9 +25,9 @@ export class PromiseState<T extends (...args: any[]) => Promise<any>, U = Return
   transform?: (value: any) => Promise<Awaited<U>> | Awaited<U> = null;
 
   autoAlert = true;
-  autoUpdate = true;
-  autoInit = true;
-  autoClean = true;
+  autoUpdate = false;
+  autoInit = false;
+  autoClean = false;
   context: any = undefined;
 
   successMsg: string = "";
