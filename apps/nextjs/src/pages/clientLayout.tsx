80d305c2c2bf3339a0f2c1f5f96fd113c62c0b81
DLHTX <949729789@qq.com>
2024-09-11 11:47:31 +0800

    refactor: update WalletProvider to use compatibleMode and remove unused dependencies

--- apps/nextjs/src/pages/clientLayout.tsx
+++ apps/nextjs/src/pages/clientLayout.tsx
@@ -4,9 +4,6 @@ import { AppProvider, RootStore } from "@dappworks/kit";
 // import { DeviceDetectStore } from "~/store/deviceDetect";
 import { ThemeProvider } from 'next-themes';
 import { WalletProvider, WalletStore } from "@dappworks/kit/wallet";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RainbowKitProvider, Wallet, connectorsForWallets, darkTheme, lightTheme } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
 import { iotex, iotexTestnet } from "viem/chains";
 
 export const ClientLayout = ({ children }: any) => {
@@ -14,7 +11,7 @@ export const ClientLayout = ({ children }: any) => {
     <div>
       <AppProvider />
       <ThemeProvider attribute="class" enableSystem={false}>
-        <WalletProvider supportedChains={[iotex, iotexTestnet]}>
+        <WalletProvider compatibleMode supportedChains={[iotex, iotexTestnet]}>
           {children}
         </WalletProvider>
       </ThemeProvider>
