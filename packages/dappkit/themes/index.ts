9b1b573b0f1ce037ea95121878e02aa49988f61d
osdodo <dodomatrix@pm.me>
2024-09-30 18:15:22 +0800

    add type tips for getStyle

--- packages/dappkit/themes/index.ts
+++ packages/dappkit/themes/index.ts
@@ -3,12 +3,15 @@ import defaultTheme from "./default.json";
 import primaryTheme from "./primary.json";
 
 export type ThemeType = "default" | "primary";
+export type StyleKeyType = keyof typeof defaultTheme;
 
-export const getStyle = (theme: ThemeType, key: string) => {
+export const getStyle = (theme: ThemeType, key: StyleKeyType) => {
   switch (theme) {
     case "default":
-      return _.get(defaultTheme, key);
+      return _.get(defaultTheme, key) || {};
     case "primary":
-      return _.get(primaryTheme, key);
+      return _.get(primaryTheme, key) || {};
+    default:
+      return {};
   }
 };
