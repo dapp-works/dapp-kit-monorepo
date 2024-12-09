b48dd750307f75213cffe645c461a2ee9edd0cd4
DLHTX <949729789@qq.com>
2024-12-09 14:25:22 +0800

    support ledger

--- packages/dappkit/wallet.ts
+++ packages/dappkit/wallet.ts
@@ -5,3 +5,4 @@ export { WalletHistoryStore, WalletRpcStore } from './module/Wallet/walletPlugin
 export { WalletConfigStore } from './module/Wallet/walletConfigStore'
 export { RpcList } from './module/Wallet/RpcList'
 export * from "./module/Wallet/type"
+export { ConnectLedger, DisconnectLedger } from './module/Wallet/ledger'
